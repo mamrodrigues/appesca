aef84baef87546e235442bab350bc19116c3b0bb
marcosmagalhaes <marcosmagalhaes.ti@gmail.com>
2016-01-12 00:10:32 -0200

    Renomeando LoginActivity para LoginUnescoActivity

--- app/src/main/java/br/org/unesco/appesca/view/LoginUnescoActivity.java
+++ app/src/main/java/br/org/unesco/appesca/view/LoginUnescoActivity.java
@@ -41,7 +41,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginUnescoActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
 
     public static Autenticado usuarioAutenticado;
@@ -276,7 +276,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
+                new ArrayAdapter<>(LoginUnescoActivity.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mEmailView.setAdapter(adapter);
@@ -344,7 +344,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                Intent intent = new Intent(LoginUnescoActivity.this, HomeActivity.class);
                 startActivity(intent);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
